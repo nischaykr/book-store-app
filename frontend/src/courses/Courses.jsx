d4801fc3cb7f2ffb6f53a3b931fcdcfacd86f206
nischaykr <nischaykumar06@gmail.com>
2024-08-25 11:41:08 +0530

    Frontend Done

---
+++ frontend/src/courses/Courses.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Course from '../components/course'
+function Courses() {
+  return (
+    <>
+        <Navbar />
+        <Course />
+        <Footer />
+    </>
+  )
+}
+
+export default Courses
